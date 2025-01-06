fc5f1ff77d6d24d1fbb133ffe200a2f5c8eaa4c8
Alfyan <alfyanshawn05@gmail.com>
2025-01-06 21:24:46 +0800

    first commit.fyan

---
+++ target/classes/displayWelcome.java
@@ -0,0 +1,6 @@
+public class displayWelcome {
+    
+    System.out.println("WELCOME TO THE CAR RENTAL MANAGEMENT SYSTEM ");
+    System.out.println("_____________________________________________");
+
+}
